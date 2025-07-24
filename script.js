10e95858f1b79eeaa5010f37ab8175d86fd5e152
Ankit Yadav <115349567+AnkitYadav0809@users.noreply.github.com>
2025-07-24 11:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function highlight() {
     //Write your code here
-
+ const boldElements = document.querySelectorAll('strong');
+    
+    // Change the color of each <strong> element to green
+    boldElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 128, 0)';
+    });
+}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+const boldElements = document.querySelectorAll('strong');
+    
+    // Change the color of each <strong> element back to black
+    boldElements.forEach(function(element) {
+        element.style.color = 'rgb(0, 0, 0)';
+    });
     
 }
